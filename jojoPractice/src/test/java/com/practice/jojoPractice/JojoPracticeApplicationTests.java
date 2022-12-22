2e7eeda18ff1b410d260df94c7739bada03b87a2
woong-sung <cjswo138@gmail.com>
2022-12-22 14:02:31 +0900

    refactor: 기본 spring 세팅

---
+++ jojoPractice/src/test/java/com/practice/jojoPractice/JojoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jojoPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
